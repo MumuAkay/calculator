e6400cd2e9a2584e5da13c1c91329aa5ff8e68f4
MumuAkay <mumu.akay@gmail.com>
2023-10-19 16:18:45 +0300

    --calculator css

---
+++ script.js
@@ -0,0 +1,21 @@
+let operation
+
+function add(a,b) {
+    operation = a + b;
+    return operation
+}
+
+function substract(a,b) {
+    operation = a - b;
+    return operation
+}
+
+function divide(a,b) {
+    operation = a / b;
+    return operation
+}
+
+function multiply(a,b) {
+    operation = a * b;
+    return operation
+}
